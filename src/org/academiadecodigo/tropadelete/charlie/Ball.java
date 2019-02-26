6684b247f3b0ba5a37ac16e48a30b0afc8f2ca9a
pedrosilvabos <pedro.silva.bos@gmail.com>
2019-02-26 20:28:02 -0100

    Back to normal

--- src/org/academiadecodigo/tropadelete/charlie/Ball.java
+++ src/org/academiadecodigo/tropadelete/charlie/Ball.java
@@ -5,24 +5,24 @@ import org.academiadecodigo.simplegraphics.graphics.Ellipse;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class Ball {
-    private int positionX;
-    private int positionY;
-    private int dX;
-    private int dY;
+
+    private int x;
+    private int y;
+    private int dx;
+    private int dy;
     private int speed;
     private Rectangle rectangle;
-
-
+    
     //initialize the ball with a motion
     //ball starts at the center of the screen
  public Ball(int x, int y){
-     this.positionX = x;
-     this.positionY = y;
+     this.x = x;
+     this.y = y;
  }
     //must know the motion of the ball
     public void move(){
-
+        x += dx;
+        y += dy;
     }
-
 
 }
--- src/org/academiadecodigo/tropadelete/charlie/Brick.java
+++ src/org/academiadecodigo/tropadelete/charlie/Brick.java
@@ -1,33 +1,45 @@
 package org.academiadecodigo.tropadelete.charlie;
 
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 public class Brick {
 
-    private int positionX;
-    private int positionY;
+    private int x;
+    private int y;
     private boolean isHit;
+    private Rectangle rectangle;
 
     public Brick(){
-        this.isHit = false;
+        rectangle = new Rectangle();
+        rectangle.setColor(Color.BLUE);
+        rectangle.fill();
     }
 
+
     public int getPositionX(){
-        return positionX;
+        return x;
     }
+
     public int getPositionY(){
-        return positionY;
+        return y;
     }
 
     public void setPositionX(int x){
-        this.positionX = x;
+        this.x = x;
     }
+
     public void setPositionY(int y){
-        this.positionY = y;
+        this.y = y;
     }
 
     public void setHit(){
         this.isHit = true;
+        rectangle.delete();
     }
+
     public void respawn(){
         this.isHit = false;
+        rectangle.fill();
     }
 }
