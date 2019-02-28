077238626069fe4d05899849897b71f363fab248
Pedro <pedro.silva.bos@gmail.com>
2019-02-28 21:51:57 -0100

    Ruben Armas merge

--- src/org/academiadecodigo/tropadelete/Block.java
+++ src/org/academiadecodigo/tropadelete/Block.java
@@ -1,45 +1,62 @@
 package org.academiadecodigo.tropadelete;
 
-import org.academiadecodigo.simplegraphics.graphics.Color;
+
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class Block {
 
-    private int x;
-    private int y;
-    private boolean isHit;
+    private int     x;
+    private int     y;
+    private int     width;
+    private int     heigth;
+    private boolean active;
     private Rectangle rectangle;
 
-    public Block(){
-        rectangle = new Rectangle();
-        rectangle.setColor(Color.BLUE);
-        rectangle.fill();
+    public Block(int x, int y, int width, int heigth) {
+        this.x = x;
+        this.y = y;
+        this.width  = width;
+        this.heigth = heigth;
+        this.active = false;
+
+
     }
 
-
-    public int getPositionX(){
+    public int getPositionX() {
         return x;
     }
 
-    public int getPositionY(){
+    public int getPositionY() {
         return y;
     }
 
-    public void setPositionX(int x){
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeigth() {
+        return this.heigth;
+    }
+
+    public void setPositionX(int x) {
         this.x = x;
     }
 
-    public void setPositionY(int y){
+    public void setPositionY(int y) {
         this.y = y;
     }
 
-    public void setHit(){
-        this.isHit = true;
-        rectangle.delete();
+    public void setActive() {
+        this.active = true;
+        //rectangle.delete();
+    }
+    public boolean isActive(){
+        return active;
     }
 
-    public void respawn(){
-        this.isHit = false;
+    public void respawn() {
+        this.active = true;
         rectangle.fill();
     }
+
 }
--- src/org/academiadecodigo/tropadelete/Stage.java
+++ src/org/academiadecodigo/tropadelete/Stage.java
@@ -1,5 +1,6 @@
 package org.academiadecodigo.tropadelete;
 
+import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
@@ -8,6 +9,10 @@ public class Stage {
     private final int PADDING = 10;
     private final int STAGE_HEIGHT = 500;
     private final int STAGE_WIDTH = 1500;
+
+
+    private final int BLOCK_WIDTH = 40;
+    private final int BLOCK_HEIGTH = 100;
 
     private final int PADDLE_WALL_OFFSET = 30;
     private final int PLAYER1_OFFSET = PADDLE_WALL_OFFSET;
@@ -40,6 +45,11 @@ public class Stage {
         player1 = new Player(PLAYER1_OFFSET, PADDING, STAGE_HEIGHT);
         player2 = new Player(PLAYER2_OFFSET, PADDING, STAGE_HEIGHT);
         new KeyboardListener(player1, player2);
+
+        // build matrix
+        blocks = blockMatrix(BLOCK_WIDTH, BLOCK_HEIGTH, 15, 8, PADDING, STAGE_WIDTH, STAGE_HEIGHT);
+        chooseBlock(60);
+        showBlocks();
     }
 
     public void start() {
@@ -56,6 +66,63 @@ public class Stage {
                 ball.draw();
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Build matrix of blocks
+     * <p>
+     * OBS: need some work, fine tunning and twiks - RA
+     */
+    public Block[] blockMatrix(int blockWidth, int blockHeigth, int blockCols, int blockRows,
+                               int padding, int canvasWidth, int canvasHeigth) {
+
+        Block[] blocks = new Block[blockCols * blockRows];
+        int i = 0;
+
+        for (int col = 0; col < blockCols; col++) {
+            int x = padding + ((canvasWidth - (blockWidth * blockCols)) / 2) + (col * blockWidth);
+
+            for (int row = 0; row < blockRows; row++) {
+                int y = padding + (row * blockHeigth);
+
+                blocks[i] = new Block(x, y, blockWidth, blockHeigth);
+                i++;
+            }
+        }
+        return blocks;
+    }
+
+    public void chooseBlock(int numBlock) {
+        int i = 0;
+
+        while (i < numBlock) {
+            int r = (int) Math.floor(Math.random() * blocks.length);
+
+            if (!blocks[r].isActive()) {
+                blocks[r].setActive();
+                i++;
+            }
+        }
+    }
+
+    public void showBlocks() {
+
+        for (int i = 0; i < blocks.length; i++) {
+            if (blocks[i].isActive()) {
+
+                // draw the block
+                Rectangle rectangle = new Rectangle(blocks[i].getPositionX(), blocks[i].getPositionY(),
+                        blocks[i].getWidth(), blocks[i].getHeigth());
+                rectangle.setColor(Color.RED); // need ramdom color here
+                rectangle.fill();
+
+                // make border around the block
+                Rectangle border = new Rectangle(blocks[i].getPositionX(), blocks[i].getPositionY(),
+                        blocks[i].getWidth(), blocks[i].getHeigth());
+                border.setColor(Color.WHITE);
+                border.draw();
             }
         }
     }
