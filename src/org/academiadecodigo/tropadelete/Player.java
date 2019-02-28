a367a69238fa9806047be33db210121d4907bf56
Pedro <pedro.silva.bos@gmail.com>
2019-02-28 01:37:29 -0100

    changed paddle thickness, added paddle skin and stage skin

--- src/org/academiadecodigo/tropadelete/Player.java
+++ src/org/academiadecodigo/tropadelete/Player.java
@@ -2,9 +2,7 @@ package org.academiadecodigo.tropadelete;
 
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
-
-import org.academiadecodigo.simplegraphics.graphics.Color;
-import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Player implements Drawable {
 
@@ -14,29 +12,36 @@ public class Player implements Drawable {
     private boolean hasLost;
 
     private Rectangle paddle;
+    private Picture paddleSkin;
     private int minHeight;
     private int maxHeight;
     private int paddleHeight;
-    private int stageHeight;
-
-    private int maxPaddleYTravel;
+    private int playerOffset;
 
     public Player(int offset, int posY, int stageHeight) {
-        this.step = 12;
+        this.step = 12; //paddle jumps
+        this.playerOffset = offset;
         this.minHeight = stageHeight;
         this.hasLost = false;
         this.paddleHeight = 150;
         this.maxHeight = stageHeight - this.paddleHeight;
         this.posY = (this.minHeight / 2) + (this.paddleHeight / 2);
-        this.paddle = generatePaddle(offset);
+        this.paddle = generatePaddle();
+        //this.paddleSkin = skinPaddle();
     }
 
+    public Rectangle generatePaddle() {
 
-    public Rectangle generatePaddle(int x) {
-
-        Rectangle paddle = new Rectangle(x, posY, 25, 150);
+        Rectangle paddle = new Rectangle(this.playerOffset, posY, 5, 150);
         paddle.setColor(Color.GREEN);
         paddle.fill();
+        return paddle;
+    }
+
+    public Picture skinPaddle() {
+        //make pictures names number and random them
+        Picture paddle = new Picture(this.playerOffset, posY, "resources/paddle.png");
+        paddle.draw();
         return paddle;
     }
 
@@ -48,22 +53,25 @@ public class Player implements Drawable {
 
             this.posY -= step;
 
-            if(posY < 13){              //CHECK THIS SHIT AND PUT IN PROPERTIES
+            if (posY < 13) {              //CHECK THIS SHIT AND PUT IN PROPERTIES
                 this.posY = 13;
 
-            }else{
+            } else {
                 this.paddle.translate(0, -step);
+                //this.paddleSkin.translate(0, -step);
             }
         }
         if (this.direction == Direction.DOWN) {
 
             this.posY += step;
             System.out.println(posY);
-            if(posY > 636){              //CHECK THIS SHIT AND PUT IN PROPERTIES
+            if (posY > 636) {              //CHECK THIS SHIT AND PUT IN PROPERTIES
                 this.posY = 636;
 
-            }else {
+            } else {
                 this.paddle.translate(0, step);
+                //this.paddleSkin.translate(0, step);
+
             }
         }
 
--- src/org/academiadecodigo/tropadelete/Stage.java
+++ src/org/academiadecodigo/tropadelete/Stage.java
@@ -1,61 +1,71 @@
 package org.academiadecodigo.tropadelete;
 
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
-
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 
 public class Stage {
-
+    //there is a bug with the width and the stage height!!
     private final int PADDING = 10;
     private final int STAGE_HEIGHT = 500;
     private final int player1_OFFSET = 30;
     private final int player2_OFFSET = 1260;
     private final Rectangle CANVAS = new Rectangle(PADDING, PADDING, 1280, 768);
 
-    private Block[] bricks;
+    private int counter = 0;
+    private Block[] blocks;
     private Player player1;
     private Player player2;
     private Ball ball;
     private boolean gameEnd;
 
-
-    public Stage() {
-        player1 = new Player(player1_OFFSET, 10, STAGE_HEIGHT);
-        player2 = new Player(player2_OFFSET, 10, STAGE_HEIGHT);
-        ball = new Ball(10, 10);
-
-    }
-
-
     public void init() {
 
         CANVAS.draw();
+        player1 = new Player(player1_OFFSET, 10, STAGE_HEIGHT);
+        player2 = new Player(player2_OFFSET, 10, STAGE_HEIGHT);
         new KeyboardListener(player1, player2);
+    }
+//this is just an idea
+    public void backgroundChanger() {
+
+            int random = (int) (Math.random() * 2);
+            System.out.println(random);
+            System.out.println("change the backgroung");
+            String[] backgrounds = {
+                    "resources/background1.jpg",
+                    "resources/background2.jpg"
+            };
+            Picture background = new Picture(PADDING, PADDING, backgrounds[random]);
+            background.draw();
+            player1.draw();
+            player2.draw();
+
     }
 
     public void start() {
 
         while (true) {
             try {
-                Thread.sleep(0);
+                Thread.sleep(1);
                 player1.move();
                 player2.move();
-
-                if (!ball.exists()) {
-                    ball =
+                if (ball == null) {
+                    ball = Utils.startBall(CANVAS);
                 }
-
+            this.counter += 1;
+                System.out.println(counter);
+                if(this.counter == 1000){
+                   // backgroundChanger();
+                    this.counter = 0;
+                }
                 ball.move();
                 ball.draw();
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
-
-
     }
-
 }
 
